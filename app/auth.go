2e352982af43fe67145a66aff800512e1f94da46
Ryan Timmons <ryanjohntimmons@gmail.com>
2019-06-22 20:23:54 -0500

    Adds Auth middleware.

--- app/auth.go
+++ app/auth.go
@@ -1,1 +1,82 @@
 package app
+
+import (
+	"net/http"
+	"strings"
+	jwt "github.com/dgrijalva/jwt-go"
+	utils "SampleApp/utils"
+	"SampleApp/models"
+	"os"
+	"context"
+	"fmt"
+)
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		unauthenticatedEndpoints := []string{"/api/user/new", "/api/user/login"}
+		requestPath := request.URL.Path
+
+		// Serve call without auth if unauthenticated endpoint.
+		for _, value := range unauthenticatedEndpoints {
+			if value == requestPath {
+				next.ServeHTTP(writer, request)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := request.Header.Get("Authorization")
+
+		// If Token Header missing, return 403 Forbidden .
+		if tokenHeader == "" {
+			response = utils.Message(false, "Missing Auth Token")
+			writer.WriteHeader(http.StatusForbidden)
+			writer.Header().Add("Content-Type", "application/json")
+			utils.Respond(writer, response)
+			return
+		}
+
+		splitTokenHeaders := strings.Split(tokenHeader, " ")
+
+		// If Token Header improperly formatted, return 403 Forbidden .
+		if len(splitTokenHeaders) != 2 {
+			response = utils.Message(false, "Invalid/Malformed Auth Token")
+			writer.WriteHeader(http.StatusForbidden)
+			writer.Header().Add("Content-Type", "application/json")
+			utils.Respond(writer, response)
+			return
+		}
+
+		tokenPart := splitTokenHeaders[1]
+		token := &models.Token{}
+
+		token, err := jwt.ParseWithClaims(tokenPart, token, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		// If Token malformed, return 403 Forbidden.
+		if err != nil {
+			response = utils.Message(false, "Malformed Authentication Token")
+			writer.WriteHeader(http.StatusForbidden)
+			writer.Header().Add("Content-Type", "application/json")
+			utils.Respond(writer, response)
+			return
+		}
+
+		// If Token invalid, return 403 Forbidden.
+		if !token.Valid {
+			response = utils.Message(false, "Token is not valid.")
+			writer.WriteHeader(http.StatusForbidden)
+			writer.Header().Add("Content-Type", "application/json")
+			utils.Respond(writer, response)
+			return
+		}
+
+		// All checks good, proceed with request.
+		fmt.Sprintf("User %", token.Username)
+		ctx := context.WithValue(request.Context(), "user", token.UserId)
+		request = request.WithContext(ctx)
+		next.ServeHTTP(writer, request)
+	})
+}
