a5e54bc017fc49529c61ea7102c27dcc14d8621a
Ryan Timmons <ryanjohntimmons@gmail.com>
2019-06-23 17:56:30 -0500

    Adds controller logic for auth.

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -1,1 +1,32 @@
 package controllers
+
+import (
+	"encoding/json"
+	"github.com/rjohnt/SampleGoApp/models"
+	"github.com/rjohnt/SampleGoApp/utils"
+	"net/http"
+)
+
+var CreateAccount = func(writer http.ResponseWriter, request *http.Request) {
+	account := &models.Account{}
+	err := json.NewDecoder(request.Body).Decode(account)
+	if err != nil {
+		utils.Respond(writer, utils.Message(false, "Invalid Request"))
+		return
+	}
+
+	response := account.Create()
+	utils.Respond(writer, response)
+}
+
+var Authenticate = func(writer http.ResponseWriter, request *http.Request) {
+	account := models.Account{}
+	err := json.NewDecoder(request.Body).Decode(account)
+	if err != nil {
+		utils.Respond(writer, utils.Message(false, "Invalid request."))
+		return
+	}
+
+	response := models.Login(account.Email, account.Password)
+	utils.Respond(writer, response)
+}
