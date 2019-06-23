ddcb15fdaa9c28032a520eb94a618b84c7707354
Ryan Timmons <ryanjohntimmons@gmail.com>
2019-06-22 20:58:30 -0500

    Adds main file.

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package SampleApp
+
+import (
+	"github.com/gorilla/mux"
+	"SampleApp/app"
+	"os"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":" + port, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
