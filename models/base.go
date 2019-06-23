53b36b057e108361ecded86ec77db07cf99da7b8
Ryan Timmons <ryanjohntimmons@gmail.com>
2019-06-23 18:17:29 -0500

    Corrects database init logic.

--- models/base.go
+++ models/base.go
@@ -18,10 +18,11 @@ func init() {
 
 	username := os.Getenv("db_user")
 	password := os.Getenv("db_pass")
+	port := os.Getenv("db_port")
 	dbName := os.Getenv("db_name")
 	dbHost := os.Getenv("db_host")
 
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+	dbUri := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, port, username, dbName, password)
 	fmt.Println(dbUri)
 
 	conn, err := gorm.Open("postgres", dbUri)
